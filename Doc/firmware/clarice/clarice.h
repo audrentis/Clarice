6fd0df2d976caf6be6b2311d4664b6941b6a20b7
Sebastian <62215791+audrentis@users.noreply.github.com>
2021-03-12 16:46:40 -0500

    mx

--- Doc/firmware/clarice/clarice.h
+++ Doc/firmware/clarice/clarice.h
@@ -22,39 +22,30 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define LAYOUT_all clarice_split_bs
 
 #define clarice_split_bs( \
-	K020,   K000, K001, K002, K003, K004, K005, K006, K010, K011, K012, K013, K014, K015, K016, K017, \
-	K040,   K021, K022, K023, K024, K025, K026,       K030, K031, K032, K033, K034, K035, K036, K037, \
-	K060,   K041, K042, K043, K044, K045, K046,       K050, K051, K052, K053, K054, K055,       K056, \
-	        K061, K062, K063, K064, K065, K066,       K070, K071, K072, K073, K074, K075, K076, K077, \
-	        K081,       K083,       K085, K086,             K091,       K093,                   K097  \
-) { \
-	{ K000,  K001,  K002,  K003,  K004,  K005,  K006,  KC_NO }, \
-	{ K010,  K011,  K012,  K013,  K014,  K015,  K016,  K017 }, \
-	{ K020,  K021,  K022,  K023,  K024,  K025,  K026,  KC_NO }, \
-	{ K030,  K031,  K032,  K033,  K034,  K035,  K036,  K037 }, \
-	{ K040,  K041,  K042,  K043,  K044,  K045,  K046,  KC_NO }, \
-	{ K050,  K051,  K052,  K053,  K054,  K055,  K056,  KC_NO }, \
-	{ K060,  K061,  K062,  K063,  K064,  K065,  K066,  KC_NO }, \
-	{ K070,  K071,  K072,  K073,  K074,  K075,  K076,  K077 }, \
-	{ KC_NO, K081,  KC_NO, K083,  KC_NO, K085,  K086,  KC_NO }, \
-	{ KC_NO, K091,  KC_NO, K093,  KC_NO, KC_NO, KC_NO, K097 }  \
+  K00, K01, K02, K03, K04, K05, K06, K07, K08, K09, K0A, K0B, K0C, K0D, K0E, K0F, \
+  K10, K11, K12, K13, K14, K15, K16,      K18, K19, K1A, K1B, K1C, K1D, K1E, K1F, \
+  K20, K21, K22, K23, K24, K25, K26,      K28, K29, K2A, K2B, K2C, K2D,      K2F, \
+       K31, K32, K33, K34, K35, K36,      K38, K39, K3A, K3B, K3C, K3D, K3E, K3F,         \
+       K41,      K43,      K45, K46,           K49,      K4B,                K4F \
+) 
+{ \
+  {  K00, K01, K02, K03, K04, K05, K06, K07, K08, K09, K0A, K0B, K0C, K0D, K0E, K0F  }, \
+  {  K10, K11, K12, K13, K14, K15, K16, KNO, K18, K19, K1A, K1B, K1C, K1D, K1E, K1F  }, \
+  {  K20, K21, K22, K23, K24, K25, K26, KNO, K28, K29, K2A, K2B, K2C, K2D, KNO, K2F  }, \
+  {  KNO, K31, K32, K33, K34, K35, K36, KNO, K38, K39, K3A, K3B, K3C, K3D, K3E, K3F  }, \
+  {  KNO, K41, KNO, K43, KNO, K45, K46, KNO, KNO, K49, KNO, K4B, KNO, KNO, KNO, K4F  }  \
 }
 
 #define LAYOUT_clarice( \
-    K020,   K000, K001, K002, K003, K004, K005, K006, K010, K011, K012, K013, K014, K015, K016,       \
-    K040,   K021, K022, K023, K024, K025, K026,       K030, K031, K032, K033, K034, K035, K036, K037, \
-    K060,   K041, K042, K043, K044, K045, K046,       K050, K051, K052, K053, K054, K055,       K056, \
-            K061, K062, K063, K064, K065, K066,       K070, K071, K072, K073, K074, K075, K076, K077, \
-            K081,       K083,       K085, K086,       K091,       K093,                         K097  \
+  K00, K01, K02, K03, K04, K05, K06, K07, K08, K09, K0A, K0B, K0C, K0D, K0E     , \
+  K10, K11, K12, K13, K14, K15, K16,      K18, K19, K1A, K1B, K1C, K1D, K1E, K1F, \
+  K20, K21, K22, K23, K24, K25, K26,      K28, K29, K2A, K2B, K2C, K2D,      K2F, \
+       K31, K32, K33, K34, K35, K36,      K38, K39, K3A, K3B, K3C, K3D, K3E, K3F,         \
+       K41,      K43,      K45, K46,           K49,      K4B,                K4F \
 ) { \
-    { K000,  K001,  K002,  K003,  K004,  K005,  K006,  KC_NO }, \
-    { K010,  K011,  K012,  K013,  K014,  K015,  K016,  KC_NO }, \
-    { K020,  K021,  K022,  K023,  K024,  K025,  K026,  KC_NO }, \
-    { K030,  K031,  K032,  K033,  K034,  K035,  K036,  K037 }, \
-    { K040,  K041,  K042,  K043,  K044,  K045,  K046,  KC_NO }, \
-    { K050,  K051,  K052,  K053,  K054,  K055,  K056,  KC_NO }, \
-    { K060,  K061,  K062,  K063,  K064,  K065,  K066,  KC_NO }, \
-    { K070,  K071,  K072,  K073,  K074,  K075,  K076,  K077 }, \
-    { KC_NO, K081,  KC_NO, K083,  KC_NO, K085,  K086,  KC_NO }, \
-    { KC_NO, K091,  KC_NO, K093,  KC_NO, KC_NO, KC_NO, K097 }  \
+  {  K00, K01, K02, K03, K04, K05, K06, K07, K08, K09, K0A, K0B, K0C, K0D, K0E, KNO  }, \
+  {  K10, K11, K12, K13, K14, K15, K16, KNO, K18, K19, K1A, K1B, K1C, K1D, K1E, K1F  }, \
+  {  K20, K21, K22, K23, K24, K25, K26, KNO, K28, K29, K2A, K2B, K2C, K2D, KNO, K2F  }, \
+  {  KNO, K31, K32, K33, K34, K35, K36, KNO, K38, K39, K3A, K3B, K3C, K3D, K3E, K3F  }, \
+  {  KNO, K41, KNO, K43, KNO, K45, K46, KNO, KNO, K49, KNO, K4B, KNO, KNO, KNO, K4F  }  \
 }
